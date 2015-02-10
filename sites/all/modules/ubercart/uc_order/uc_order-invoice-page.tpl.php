87091b65a32ebda5b3229484c7c1944d0c052dbb
debasiskar007 <debasiskar007@gmail.com>
2015-02-10 15:53:44 +0530

    15.53 feb 10th

--- sites/all/modules/ubercart/uc_order/uc_order-invoice-page.tpl.php
+++ sites/all/modules/ubercart/uc_order/uc_order-invoice-page.tpl.php
@@ -12,7 +12,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 
 <head>
-  <?php print $head; ?>
+  <?php //echo "test";
+  print $head; ?>
   <title><?php print $head_title; ?></title>
   <style type="text/css">
     .buttons {
@@ -31,6 +32,7 @@
     <input type="button" value="<?php print t('Print invoice'); ?>" onclick="window.print();" />
   </div>
 
-  <?php print $content; ?>
+  <?php
+  print $content; ?>
 </body>
 </html>
